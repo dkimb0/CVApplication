d6f401b92a0ee5a9f4341c2057fbbb2da781c1d7
dkimb0 <jhdavidk@gmail.com>
2024-02-16 12:35:38 -0500

    set up header formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 
-// function Input({title, placeholder, handleChange, fullName, onFullNameChange}){
-//   return(
-//     <>
-//       <label htmlFor="">{title}</label>
-//       <input
-//         type="text"
-//         value={fullName}
-//         placeholder={placeholder}
-//         onChange={(e) => onFullNameChange(e.target.value)}/>
-//     </>
-//   )
-// }
-
-function TextAreaInput({title, placeholder}){
-  return(
-    <>
-      <label htmlFor="">{title}</label>
-      <textarea placeholder={placeholder} />
-    </>
-  )
-}
 
 function Panel({ title, children, handleEducationSubmit }){
   const [isActive, setIsActive] = useState(false);
@@ -42,110 +21,24 @@ function Panel({ title, children, handleEducationSubmit }){
   )
 }
 
-function PersonalDetailsPanel({ title, children }){
-  return(
-    <section className='panel'>
-      <h1>{title}</h1>
-      <form>
-        {children}
-      </form>
-    </section>
-  )
-}
-
-
-function PersonalDetails({ fullName, onFullNameChange, email, onEmailChange,
-  phoneNumber, onPhoneNumberChange, address, onAddressChange }) {
-  return(
-    <section className='panel'>
-      <h1>Personal Details</h1>
-      <form>
-        <label>Full Name</label>
-        <input type="text" placeholder='John Doe' value = { fullName } 
-          onChange={(e) => onFullNameChange(e.target.value)} />
-
-        <label>Email</label>
-        <input type="text" placeholder='example@gmail.com' value={email}
-          onChange={(e) => onEmailChange(e.target.value)}/>
-
-        <label>Phone Number</label>
-        <input type="tel" placeholder='(555) 555-5555' value={phoneNumber}
-          onChange={(e) => onPhoneNumberChange(e.target.value)}/>
-
-        <label>Address</label>
-        <input type="text" placeholder='Brooklyn, NY' value={address}
-          onChange={(e) => onAddressChange(e.target.value)}/>
-      </form>
-    </section>
-  )
-}
-
-function Education(education, handleEducationChange, handleEducationSubmit) {
-  return(
-    <Panel title='Education' handleEducationSubmit={handleEducationSubmit}>
-      <label htmlFor='degree'>Degree</label>
-      <input type='text' id="degree" name="degree" placeholder='Software Engineering' value={education.degree}
-        onChange = {handleEducationChange}/>
-      {/* <Input title='School' placeholder='Columbia University'/>
-      <Input title='City' placeholder='New York'/>
-      <Input title='Country' placeholder='USA'/>
-      <Input title='Start Date' placeholder='mm/yyyy'/>
-      <Input title='End Date' placeholder='mm/yyyy'/> */}
-    </Panel>
-  )
-}
-
-function Experience() {
-  return(
-    <Panel title="Experience">
-      <Input title='Company Name' placeholder='Google'/>
-      <Input title='Position Title' placeholder='Full Stack Developer'/>
-      <Input title='Location' placeholder='New York'/>
-      <Input title='Start Date' placeholder='mm/yyyy'/>
-      <Input title='End Date' placeholder='mm/yyyy'/>      
-      <TextAreaInput title='Description' placeholder=''/>
-    </Panel>
-  )
-}
-
-function Sidebar({ fullName, onFullNameChange, email, onEmailChange,
-  phoneNumber, onPhoneNumberChange, address, onAddressChange,
-  handleEducationChange, handleEducationSubmit, education }) {
-  return(
-    <div className='sidebar'>
-      <PersonalDetails
-        fullName = { fullName }
-        onFullNameChange = { onFullNameChange }
-        email = { email }
-        onEmailChange= { onEmailChange }
-        phoneNumber = { phoneNumber }
-        onPhoneNumberChange = {onPhoneNumberChange}
-        address={address}
-        onAddressChange={onAddressChange}
-        />
-
-      <Education
-        education = {education}
-        handleEducationChange = {handleEducationChange}
-        handleEducationSubmit = {handleEducationSubmit}
-      />
-      {/* <Experience /> */}
-    </div>
-  )
-}
-
 function Resume({fullName, email, phoneNumber, address}){
   return(
     <div className='resume'>
       <header className='personalDetails'>
-        <p>{fullName}</p>
-        <p>{email}</p>
-        <p>{phoneNumber}</p>
-        <p>{address}</p>
+        <p className='fullName'>{fullName}</p>
+        <section className='contactInfo'>
+          <span className='email'>{email}</span>
+          <span className='phoneNumber'>{phoneNumber}</span>
+          <span className='address'>{address}</span>
+        </section>
       </header>
+      
       <section className='education'>
+          
       </section>
+      
       <section className='experience'>
+      
       </section>
     </div>
   )
@@ -222,6 +115,17 @@ function App() {
             onChange = {handleEducationChange}
           />
         </Panel>
+
+        <Panel title="Experience">
+          <label>Company Name</label>
+          <input title='Company Name' placeholder='Google'/>
+          <input title='Position Title' placeholder='Full Stack Developer'/>
+          <input title='Location' placeholder='New York'/>
+          <input title='Start Date' placeholder='mm/yyyy'/>
+          <input title='End Date' placeholder='mm/yyyy'/>      
+          <label>Description</label>
+          <textarea placeholder='Description' />
+        </Panel>
       </div>
 
 
@@ -230,6 +134,8 @@ function App() {
         email={email}
         phoneNumber={phoneNumber}
         address={address}
+
+
       >
 
       </Resume>
