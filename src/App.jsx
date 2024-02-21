615b527cc727cb38a707ab940e51a596e169252f
dkimb0 <jhdavidk@gmail.com>
2024-02-21 02:49:50 -0500

    fixed edit buttons to function properly

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,78 @@ import { useState } from 'react'
 import './App.css'
 
 
-function Panel({ title, children, handleEducationSubmit }){
+function Panel({ title, children, education, setEducation, addEducation, inputArray, deleteItem, handleEdit, educationArray }){
   const [isActive, setIsActive] = useState(false);
+  const [formActive, setFormActive] = useState(false);
+  const [formEdit, setFormEdit] = useState(false);
+  const [indexOfEdit, setIndexOfEdit] = useState(0)
 
+  const handleEducationSubmit = (e) => {
+    e.preventDefault();
+    if(formEdit){
+      handleEdit(indexOfEdit);
+      
+    }else{
+      addEducation(education);
+    }
+    setEducation([]);
+    setFormActive(false);
+  }
+  
   return(
     <section className='panel'>
       <h1>{title}</h1>
-      {isActive ? (
+      {formActive ? (
+        <form onSubmit={handleEducationSubmit}>
+          {children}
+          <button type='submit'>Save</button>
+          <button onClick={() => {
+            setFormActive(false);
+            setEducation([]);
+            }}>Cancel</button>
+        </form>    
+      ) : (
+        <>
+          <ul>
+            {inputArray.map(education => {
+              return(
+                  <li key={education.idForm}>
+                    <span>{education.school}</span>
+                    <button onClick={() => {
+                      setFormEdit(true);
+                      const index = educationArray.findIndex(edu => edu.idForm === education.idForm);
+                      setIndexOfEdit(index)
+                      setEducation(educationArray[index]);
+                      setFormActive(true);
+                    }}>🖊️</button>
+                    <button onClick={() => deleteItem(education.idForm)}>🗑️</button>
+                  </li>
+              )
+            })}
+          </ul>
+          <button onClick={() => {
+            setFormEdit(false);
+            setFormActive(true);
+            }}>+ Education</button>
+        </>
+      )}
+      
+
+      {/* {isActive ? (
         <form onSubmit={handleEducationSubmit}>
           <button onClick={() => setIsActive(false)}>Hide</button>
           {children}
           <button type='submit'>Submit</button>
         </form>
       ) : (
-        <button onClick={() => setIsActive(true)}>Show</button>
-      )}
+        <button onClick={()
+           => setIsActive(true)}>Show</button>
+      )} */}
     </section>
   )
 }
 
-function Resume({fullName, email, phoneNumber, address}){
+function Resume({fullName, email, phoneNumber, address, educationArray}){
   return(
     <div className='resume'>
       <header className='personalDetails'>
@@ -31,11 +83,22 @@ function Resume({fullName, email, phoneNumber, address}){
           <span className='phoneNumber'>{phoneNumber}</span>
           <span className='address'>{address}</span>
         </section>
+
       </header>
       
-      <section className='education'>
-          
-      </section>
+      <ul className='education'>
+        {educationArray.map(education => {
+          return(
+            <li key={education.idResume}>
+              <span>{education.school}</span>
+              <span>{education.degree}</span>
+              <span>{education.location}</span>
+              <span>{education.startDate}</span>
+              <span>{education.endDate}</span>
+            </li>
+          )
+        })}
+      </ul>
       
       <section className='experience'>
       
@@ -44,25 +107,39 @@ function Resume({fullName, email, phoneNumber, address}){
   )
 }
 
-function App() {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [address, setAddress] = useState('');
+export default function App() {
+  const [personalDetails, setPersonalDetails] = useState({fullName: '', email: '', phoneNumber: '',
+    address: ''});
+  const [educationArray, setEducationArray] = useState([]);
+  const [education, setEducation] = useState({});
 
-  const [education, setEducation] = useState({degree: '', school: '', city: '', country: '',
-    startDate: '', endDate: ''});
-
-  const handleEducationChange = (e) => {
-    const {name, value} = e.target;
-    setEducation(values => ({...values, [name]: value}));
-  };
-
-  const handleEducationSubmit = (e) => {
-    e.preventDefault();
-    alert(education.degree);
-    
+  function addEducation(education){
+    setEducationArray(currentEducationArray => {
+      return [
+        ...currentEducationArray,  {...education, idResume: crypto.randomUUID(), idForm: crypto.randomUUID()}
+      ]
+    })
   }
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setEducation(currentEducation => ({...currentEducation, [name]: value}));
+  }
+
+  const deleteItem = (id) => {
+    setEducationArray(currentArray => {
+      return currentArray.filter(item => item.idForm !== id);
+    })
+  }
+
+  const handleEdit = (index) => {
+    setEducationArray(currentEducationArray => {
+      currentEducationArray[index] = { ...education, idResume:crypto.randomUUID(), idForm: crypto.randomUUID() };
+      return currentEducationArray
+    })
+    // setEducation([]);
+  }
+
 
   return(
     <>
@@ -71,52 +148,48 @@ function App() {
           <h1>Personal Details</h1>
           <form>
             <label>Full Name</label>
-            <input type="text" placeholder='John Doe' value = { fullName } 
-              onChange={(e) => setFullName(e.target.value)} />
+            <input type="text" placeholder='John Doe' value = { personalDetails.fullName } 
+              onChange={(e) => setPersonalDetails({...personalDetails, fullName: e.target.value})} />
 
             <label>Email</label>
-            <input type="text" placeholder='example@gmail.com' value={email}
-              onChange={(e) => setEmail(e.target.value)}/>
+            <input type="text" placeholder='example@gmail.com' value={personalDetails.email}
+              onChange={(e) => setPersonalDetails({...personalDetails, email: e.target.value})}/>
 
             <label>Phone Number</label>
-            <input type="tel" placeholder='(555) 555-5555' value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}/>
+            <input type="text" placeholder='(555) 555-5555' value={personalDetails.phoneNumber}
+              onChange={(e) => setPersonalDetails({...personalDetails, phoneNumber: e.target.value})}/>
 
             <label>Address</label>
-            <input type="text" placeholder='Brooklyn, NY' value={address}
-              onChange={(e) => setAddress(e.target.value)}/>
+            <input type="text" placeholder='Brooklyn, NY' value={personalDetails.address}
+              onChange={(e) => setPersonalDetails({...personalDetails, address: e.target.value})}/>
           </form>
         </section>
 
-        <Panel title='Education' handleEducationSubmit={handleEducationSubmit}>
+        <Panel title='Education' education={education} setEducation={setEducation} addEducation={addEducation}
+          inputArray={educationArray} deleteItem={deleteItem} handleEdit={handleEdit} educationArray ={educationArray}>
           <label htmlFor='school'>School</label>
           <input type='text' id='school' name='school' placeholder='Columbia University'
-            value={education.school}
-            onChange = {handleEducationChange}
+            onChange={handleChange} defaultValue={education.school || ''}
           />
           <label htmlFor='degree'>Degree</label>
           <input type='text' id="degree" name="degree" placeholder='Software Engineering'
-            value={education.degree}
-            onChange = {handleEducationChange}
+            onChange = {handleChange} defaultValue={education.degree || ''}
           />
           <label htmlFor='location'>Location</label>
           <input type='text' id='location' name='location' placeholder='New York'
-            value={education.location}
-            onChange = {handleEducationChange}
+            onChange = {handleChange} defaultValue={education.location || ''}
           />
           <label htmlFor='startDate'>Start Date</label>
           <input type='text' id='startDate' name='startDate' placeholder='mm/yyyy'
-            value={education.startDate}
-            onChange = {handleEducationChange}
+            onChange = {handleChange} defaultValue={education.startDate || ''}
           />
           <label htmlFor='endDate'>End Date</label>
           <input type='text' id='endDate' name='endDate' placeholder='mm/yyyy'
-            value={education.endDate}
-            onChange = {handleEducationChange}
+            onChange = {handleChange} defaultValue={education.endDate || ''}
           />
         </Panel>
 
-        <Panel title="Experience">
+        {/* <Panel title="Experience">
           <label>Company Name</label>
           <input title='Company Name' placeholder='Google'/>
           <input title='Position Title' placeholder='Full Stack Developer'/>
@@ -125,15 +198,16 @@ function App() {
           <input title='End Date' placeholder='mm/yyyy'/>      
           <label>Description</label>
           <textarea placeholder='Description' />
-        </Panel>
+        </Panel> */}
       </div>
 
 
       <Resume
-        fullName={fullName}
-        email={email}
-        phoneNumber={phoneNumber}
-        address={address}
+        fullName={personalDetails.fullName}
+        email={personalDetails.email}
+        phoneNumber={personalDetails.phoneNumber}
+        address={personalDetails.address}
+        educationArray = {educationArray}
 
 
       >
@@ -142,5 +216,3 @@ function App() {
     </>
   )
 }
-
-export default App
